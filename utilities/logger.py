f0b1db2eddad1f8c32228d73c7a48ac04502a854
Tom Heng <uestcheng@gmail.com>
2017-07-17 23:25:09 +0800

    start the begin part of logger and complete the info logging part

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,2 +1,25 @@
 import logging
 
+
+class Logger(object):
+    """Logger class doing two things
+       1. print out to the console
+       2. loggin to the file in the output
+
+    """
+
+    def __init__(self, log_level, way='print'):
+        self.fmt = "%(asctime)s-%(levelname)s: %(name)s-%(message)s"
+        self.way = way
+        self.level = log_level
+        self.level_type = ("DEBUG", "CRITICAL", "WARNING")
+
+    def info_log(self, msg):
+        """log common info of test"""
+        if self.level not in self.level_type:
+            logging.basicConfig(self.fmt)
+            getattr(logging, self.level.lower())(msg)
+
+    def error_log(self, msg):
+        """log error info of test"""
+        pass
